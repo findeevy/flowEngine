22aac1c95444fe95a27e6912310babd149ba4ae3
findeevy <findeevy@gmail.com>
2026-05-30 00:07:17 -0500

    cameraWork

--- src/scene/camera.h
+++ src/scene/camera.h
@@ -1,1 +1,15 @@
-//placeholder
+#pragma once
+
+include <glm/vec3.hpp>
+
+class Camera{
+  public:
+    //settings
+    float fov = 90.0f;
+    float nearClip = 0.001f;
+    float farClip = 1000.0f;
+
+    //location
+    glm::vec3 position{0.0f, 0.0f, 0.0f};
+    glm::quat rotation{1.0f, 0.0f, 0.0f, 0.0f};
+};
